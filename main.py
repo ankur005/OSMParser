93991caea70849c51ff1d097b3290e5dd63cb3fb
ankur005 <bansalankur10@gmail.com>
2016-10-17 13:36:10 +0800

    Each node pair identified as separate road link

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import math
 tree = xml.parse("C:\Users\Ankur Bansal\Desktop\singapore.osm")
 root = tree.getroot()
 nodes = {}
+osmIds = {}
+osm = 1
 nodeId = 1000
 def longtoInt(node):
     global nodeId, nodes
@@ -15,6 +17,16 @@ def longtoInt(node):
         nodeId += 1
         nodes[node] = nodeId
         return nodeId
+
+def mapOsmId(osm_id):
+    global osmIds,osm
+    if osm_id in osmIds:
+        return osmIds[osm]
+    else:
+        osm+=1
+        osmIds[osm_id] = osm
+        return osm
+
 
 def dbConnect():
     conn = psycopg2.connect(database="postgres", user="postgres", password="password", host="127.0.0.1", port="5432")
@@ -45,31 +57,36 @@ def parser():
                     tags[child.attrib['k']] = child.attrib['v']
             ways[long(wayId)] = {"nodes" : wayNodes, "tags" : tags}
 
-    id = 0
     for key,way in ways.iteritems():
         clazz = way['tags'].get('highway')
         if clazz:
-            id += 1
             osm_id = key
+            id = mapOsmId(osm_id)
             osm_name = way['tags'].get('name')
             if(type(osm_name) == unicode):
                 osm_name = osm_name.encode("utf-8")
-            osm_source_id = way['nodes'][0]
-            osm_target_id = way['nodes'][-1]
-            source = int(longtoInt(osm_source_id))
-            target = int(longtoInt(osm_target_id))
-            kmh = int(re.findall('\d+',way['tags'].get('maxspeed',"40"))[0])
-            x1 = float(nodes[osm_source_id]['lat'])
-            y1 = float(nodes[osm_source_id]['lon'])
-            x2 = float(nodes[osm_target_id]['lat'])
-            y2 = float(nodes[osm_target_id]['lon'])
-            km = math.sqrt(math.pow((x1 - x2),2) + math.pow((y1 - y2),2))
-            data = (id, osm_id, osm_name, osm_source_id, osm_target_id, clazz, source, target, km, kmh, x1, y1, x2, y2)
-            cur.execute(query,data)
-            if not(way['tags'].get('oneway') == "yes"):
-                id += 1
-                data = (id, osm_id, osm_name, osm_target_id, osm_source_id, clazz, source, target, km, kmh, x2, y2, x1, y1)
+
+            for i in range(len(way['nodes']) - 1):
+                osm_source_id = way['nodes'][i]
+                osm_target_id = way['nodes'][i+1]
+                source = int(longtoInt(osm_source_id))
+                target = int(longtoInt(osm_target_id))
+                kmh = int(re.findall('\d+', way['tags'].get('maxspeed', "40"))[0])
+                x1 = float(nodes[osm_source_id]['lat'])
+                y1 = float(nodes[osm_source_id]['lon'])
+                x2 = float(nodes[osm_target_id]['lat'])
+                y2 = float(nodes[osm_target_id]['lon'])
+                km = math.sqrt(math.pow((x1 - x2), 2) + math.pow((y1 - y2), 2))
+                data = (
+                id, osm_id, osm_name, osm_source_id, osm_target_id, clazz, source, target, km, kmh, x1, y1, x2, y2)
                 cur.execute(query, data)
+                if not (way['tags'].get('oneway') == "yes"):
+                    data = (
+                    id, osm_id, osm_name, osm_target_id, osm_source_id, clazz, source, target, km, kmh, x2, y2, x1, y1)
+                    cur.execute(query, data)
+            # osm_source_id = way['nodes'][0]
+            # osm_target_id = way['nodes'][-1]
+
 
     conn.commit()
     conn.close()
